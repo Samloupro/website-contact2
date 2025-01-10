292464be5d37dd30bf97bbeb42876e45e59ddd47
Samloupro <54007076+Samloupro@users.noreply.github.com>
2025-01-10 20:28:26 +0100

    Update phone_extractor.py

--- utils/phone_extractor.py
+++ utils/phone_extractor.py
@@ -1,1 +1,20 @@
+import re
+
+def validate_phones(phones):
+    return [phone for phone in phones if 10 <= len(re.sub(r'\D', '', phone)) <= 15]
+
+def extract_phones_html(text):
+    return re.findall(r'\+?[0-9][0-9.\-\s()]{8,}[0-9]', text)
+
+def extract_phones_jsonld(soup):
+    phones = []
+    scripts = soup.find_all("script", type="application/ld+json")
+    for script in scripts:
+        try:
+            data = json.loads(script.string)
+            if "telephone" in data:
+                phones.append(data["telephone"])
+        except (json.JSONDecodeError, TypeError):
+            continue
+    return phones
 
