928dbf149e139aec381f1140dd8e842b9b2f4522
Samloupro <54007076+Samloupro@users.noreply.github.com>
2025-01-10 20:55:42 +0100

    Update link_explorer.py

--- utils/link_explorer.py
+++ utils/link_explorer.py
@@ -1,1 +1,10 @@
+def extract_links(soup, base_url):
+    links = set()
+    for a_tag in soup.find_all("a", href=True):
+        href = a_tag["href"]
+        if href.startswith("/"):
+            href = base_url.rstrip("/") + href
+        if href.startswith("http"):
+            links.add(href)
+    return links
 
